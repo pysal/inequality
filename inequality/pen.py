c14a1b90864fe75d48187df8162d9d1f9ffabd11
Serge Rey <sjsrey@gmail.com>
2024-07-21 07:01:01 -0700

    ENH: add query to pengram

--- inequality/pen.py
+++ inequality/pen.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import mapclassify as mc
+import matplotlib.patches as patches
 
 
 def pen(df, col, x, ascending=True,
@@ -28,16 +29,9 @@ def pen(df, col, x, ascending=True,
     return fig
 
 
-def pengram(gdf,
-            col,
-            x,
-            figsize=(8, 6), k=5,
-            scheme='Quantiles',
-            xticks=True,
-            leg_pos="lower right",
-            orientation='r',
-            fmt="{:.2f}",
-            ratio=[3, 1]):
+def pengram(gdf, col, name, figsize=(8, 6), k=5, scheme='Quantiles', xticks=True,
+            leg_pos="lower right", orientation='r', fmt="{:.2f}", ratio=[3, 1],
+            query=[]):
 
     if orientation == 'r':
         nrow = 1
@@ -48,42 +42,45 @@ def pengram(gdf,
         ncol = 1
         dim_ratios = 'height_ratios'
 
-
     fig, (ax1, ax2) = plt.subplots(nrow, ncol, figsize=figsize,
                                    gridspec_kw={dim_ratios: ratio})
 
-    # Plot the map on the first axis
-    gdf.plot(column=col, scheme=scheme, k=k, ax=ax1, legend=True,
-             legend_kwds={'loc': leg_pos,
-                          'fmt': fmt},
-             )
+    base = gdf.plot(column=col, scheme=scheme, k=k, ax=ax1, legend=True,
+                    legend_kwds={'loc': leg_pos, 'fmt': fmt})
     ax1.axis('off')
 
-    # Classify the data
+    if query:
+        highlight = gdf[gdf[name].isin(query)]
+        highlight.boundary.plot(ax=ax1, edgecolor='red', linewidth=2)
+
     binned = mc.Quantiles(gdf[col], k=k)
     gdf['_bin'] = binned.yb
 
-    # Sort the GeoDataFrame by the specified column
     sgdf = gdf.sort_values(by=col, ascending=True).reset_index(drop=True)
 
-    # Create the bar plot on the second axis
-    sns.barplot(x=sgdf.index, y=col, hue='_bin',
-                data=sgdf, palette='viridis', ax=ax2)
+    sns.barplot(x=sgdf.index, y=col, hue='_bin', data=sgdf, palette='viridis', ax=ax2)
     ax2.set_ylabel(col)
-    ax2.set_xlabel(x)
+    ax2.set_xlabel(name)
     plt.xticks(rotation=90)
     ax2.set_title("Pen's Parade")
 
     ax2.set_xticks(sgdf.index)
-    ax2.set_xticklabels(sgdf[x], rotation=90)
-    # Optionally, remove the x-axis labels
+    ax2.set_xticklabels(sgdf[name], rotation=90)
+
     if not xticks:
         ax2.set(xticks=[])
         ax2.set(xlabel='')
 
-    # Remove the legend from the barplot
+    if query:
+        for obs in query:
+            if obs in sgdf[name].values:
+                obs_idx = sgdf[sgdf[name] == obs].index[0]
+                rect = patches.Rectangle((obs_idx - 0.5, 0), 1, sgdf.loc[obs_idx, col],
+                                         linewidth=2, edgecolor='red', facecolor='none')
+                ax2.add_patch(rect)
+
     ax2.get_legend().remove()
 
-    # Adjust layout for better visualization
     plt.tight_layout()
     return fig
+
