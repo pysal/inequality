de6b0718dcd4884c49bcc2a4c46356c3a3cc8691
Serge Rey <sjsrey@gmail.com>
2024-08-15 08:32:35 -0700

    Conditional dependencies for pengram

--- inequality/pen.py
+++ inequality/pen.py
@@ -14,17 +14,14 @@ Author
 Serge Rey <srey@sdsu.edu>
 """
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-import mapclassify as mc
-import matplotlib.patches as patches
 import math
-import pandas as pd
+
 import numpy as np
 
 
-def pen(df, col, x, weight=None, ascending=True, xticks=True,
-        total_bars=100, figsize=[8, 6]):
+def pen(
+    df, col, x, weight=None, ascending=True, xticks=True, total_bars=100, figsize=[8, 6]
+):
     """
     Creates the Pen's Parade visualization.
 
@@ -60,6 +57,27 @@ def pen(df, col, x, weight=None, ascending=True, xticks=True,
         A Matplotlib figure object with the Pen's Parade plot.
 
     """
+    try:
+        import seaborn as sns
+    except ImportError:
+        raise ImportError(
+            "Seaborn is required for pen. Please install it using 'pip install seaborn'."
+        )
+
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        raise ImportError(
+            "Matplotlib is required for pen. Please install it using 'pip install matplotlib'."
+        )
+
+    try:
+        import pandas as pd
+    except ImportError:
+        raise ImportError(
+            "Pandas is required for pen. Please install it using 'pip install pandas'."
+        )
+
     if weight is None:
         fig, ax = plt.subplots(1, 1, figsize=figsize)
         dbfs = df.sort_values(col, ascending=ascending).reset_index(drop=True)
@@ -72,25 +90,26 @@ def pen(df, col, x, weight=None, ascending=True, xticks=True,
 
         if not xticks:
             ax.set(xticks=[])
-            ax.set(xlabel='')
+            ax.set(xlabel="")
 
         plt.tight_layout()
         return fig
     else:
-        df['NumBars'] = (df[weight] / df[weight].sum() *
-                         total_bars).apply(math.ceil).astype(int)
+        df["NumBars"] = (
+            (df[weight] / df[weight].sum() * total_bars).apply(math.ceil).astype(int)
+        )
 
         repeated_rows = []
         name = x
         for _, row in df.iterrows():
-            repeated_rows.extend([row] * row['NumBars'])
+            repeated_rows.extend([row] * row["NumBars"])
 
         df_repeated = pd.DataFrame(repeated_rows)
 
         df_sorted = df_repeated.sort_values(by=col).reset_index(drop=True)
 
         unique_obs = df[name].unique()
-        colors = plt.cm.get_cmap('tab20', len(unique_obs))
+        colors = plt.cm.get_cmap("tab20", len(unique_obs))
         color_map = {state: colors(i) for i, state in enumerate(unique_obs)}
         bar_colors = df_sorted[name].map(color_map)
 
@@ -100,9 +119,14 @@ def pen(df, col, x, weight=None, ascending=True, xticks=True,
         bar_heights = df_sorted[col]
         bar_widths = 1  # Equal width for all bars
 
-        _ = ax.bar(bar_positions, bar_heights, width=bar_widths,
-                   color=bar_colors, edgecolor='black')
-        tick_width = plt.rcParams['xtick.major.width']
+        _ = ax.bar(
+            bar_positions,
+            bar_heights,
+            width=bar_widths,
+            color=bar_colors,
+            edgecolor="black",
+        )
+        tick_width = plt.rcParams["xtick.major.width"]
 
         first_positions = []
         first_labels = []
@@ -117,12 +141,20 @@ def pen(df, col, x, weight=None, ascending=True, xticks=True,
                     first_labels.append(df_sorted[name].iloc[i])
                 else:
                     text_y_position = bar_heights[i] + 0.05 * max(bar_heights)
-                    ax.plot([bar_positions[i], bar_positions[i]],
-                            [bar_heights[i], text_y_position - 550],
-                            color='black', linewidth=tick_width)
-                    ax.text(bar_positions[i], text_y_position,
-                            df_sorted[name].iloc[i], ha='center',
-                            rotation=90, fontsize=8)
+                    ax.plot(
+                        [bar_positions[i], bar_positions[i]],
+                        [bar_heights[i], text_y_position - 550],
+                        color="black",
+                        linewidth=tick_width,
+                    )
+                    ax.text(
+                        bar_positions[i],
+                        text_y_position,
+                        df_sorted[name].iloc[i],
+                        ha="center",
+                        rotation=90,
+                        fontsize=8,
+                    )
                 current_state = df_sorted[name].iloc[i]
                 state_index += 1
 
@@ -131,16 +163,25 @@ def pen(df, col, x, weight=None, ascending=True, xticks=True,
 
         ax.set_xlabel(name)
         ax.set_ylabel(col)
-        ax.set_title(f'Weighted Pen Parade of {name} by {col}')
+        ax.set_title(f"Weighted Pen Parade of {name} by {col}")
 
-        # plt.show()
         return fig
 
 
-def pengram(gdf, col, name, figsize=(8, 6), k=5,
-            scheme='Quantiles', xticks=True, leg_pos="lower right",
-            orientation='r', fmt="{:.2f}", ratio=[3, 1],
-            query=[]):
+def pengram(
+    gdf,
+    col,
+    name,
+    figsize=(8, 6),
+    k=5,
+    scheme="quantiles",
+    xticks=True,
+    leg_pos="lower right",
+    orientation="r",
+    fmt="{:.2f}",
+    ratio=[3, 1],
+    query=[],
+):
     """
     Pen's Parade combined with a choropleth map.
 
@@ -162,8 +203,7 @@ def pengram(gdf, col, name, figsize=(8, 6), k=5,
     k : int, optional
         Number of classes for the classification scheme. Default is 5.
     scheme : str, optional
-        Classification scheme to use (e.g., 'Quantiles'). Default is
-        'Quantiles'.
+        Classification scheme to use (e.g., 'Quantiles'). Default is 'quantiles'.
     xticks : bool, optional
         Whether to show x-axis ticks. Default is True.
     leg_pos : str, optional
@@ -185,34 +225,80 @@ def pengram(gdf, col, name, figsize=(8, 6), k=5,
         The function generates and displays a plot.
 
     """
-    if orientation == 'r':
+    try:
+        import seaborn as sns
+    except ImportError:
+        raise ImportError(
+            "Seaborn is required for pengram. Please install it using 'pip install seaborn'."
+        )
+    try:
+        import geopandas as gpd
+    except ImportError:
+        raise ImportError(
+            "GeoPandas is required for pengram. Please install it using 'pip install geopandas'."
+        )
+
+    try:
+        import mapclassify as mc
+    except ImportError:
+        raise ImportError(
+            "mapclassify is required for pengram. Please install it using 'pip install mapclassify'."
+        )
+
+    try:
+        import matplotlib.patches as patches
+        import matplotlib.pyplot as plt
+    except ImportError:
+        raise ImportError(
+            "Matplotlib is required for pengram. Please install it using 'pip install matplotlib'."
+        )
+
+    try:
+        import mapclassify as mc
+    except ImportError:
+        raise ImportError(
+            "Mapclassify is required for pengram. Please install it using 'pip install mapclassify'."
+        )
+    try:
+        import pandas as pd
+    except ImportError:
+        raise ImportError(
+            "Pandas is required for pengram. Please install it using 'pip install pandas'."
+        )
+
+    if orientation == "r":
         nrow = 1
         ncol = 2
-        dim_ratios = 'width_ratios'
-    elif orientation == 'b':
+        dim_ratios = "width_ratios"
+    elif orientation == "b":
         nrow = 2
         ncol = 1
-        dim_ratios = 'height_ratios'
+        dim_ratios = "height_ratios"
 
-    fig, (ax1, ax2) = plt.subplots(nrow, ncol, figsize=figsize,
-                                   gridspec_kw={dim_ratios: ratio})
+    fig, (ax1, ax2) = plt.subplots(
+        nrow, ncol, figsize=figsize, gridspec_kw={dim_ratios: ratio}
+    )
 
-    _ = gdf.plot(column=col, scheme=scheme, k=k, ax=ax1, legend=True,
-                 legend_kwds={'loc': leg_pos, 'fmt': fmt})
-    ax1.axis('off')
+    _ = gdf.plot(
+        column=col,
+        scheme=scheme,
+        k=k,
+        ax=ax1,
+        legend=True,
+        legend_kwds={"loc": leg_pos, "fmt": fmt},
+    )
+    ax1.axis("off")
 
     if query:
         highlight = gdf[gdf[name].isin(query)]
-        highlight.boundary.plot(ax=ax1, edgecolor='red', linewidth=2)
+        highlight.boundary.plot(ax=ax1, edgecolor="red", linewidth=2)
 
-    # binned = mc.Quantiles(gdf[col], k=k)
     binned = mc.classify(gdf[col], scheme, k=k)
-    gdf['_bin'] = binned.yb
+    gdf["_bin"] = binned.yb
 
     sgdf = gdf.sort_values(by=col, ascending=True).reset_index(drop=True)
 
-    sns.barplot(x=sgdf.index, y=col, hue='_bin',
-                data=sgdf, palette='viridis', ax=ax2)
+    sns.barplot(x=sgdf.index, y=col, hue="_bin", data=sgdf, palette="viridis", ax=ax2)
     ax2.set_ylabel(col)
     ax2.set_xlabel(name)
     plt.xticks(rotation=90)
@@ -223,18 +309,23 @@ def pengram(gdf, col, name, figsize=(8, 6), k=5,
 
     if not xticks:
         ax2.set(xticks=[])
-        ax2.set(xlabel='')
+        ax2.set(xlabel="")
 
     if query:
         for obs in query:
             if obs in sgdf[name].values:
                 obs_idx = sgdf[sgdf[name] == obs].index[0]
-                rect = patches.Rectangle((obs_idx - 0.5, 0), 1,
-                                         sgdf.loc[obs_idx, col],
-                                         linewidth=2, edgecolor='red',
-                                         facecolor='none')
+                rect = patches.Rectangle(
+                    (obs_idx - 0.5, 0),
+                    1,
+                    sgdf.loc[obs_idx, col],
+                    linewidth=2,
+                    edgecolor="red",
+                    facecolor="none",
+                )
                 ax2.add_patch(rect)
 
     ax2.get_legend().remove()
 
     plt.tight_layout()
+    return fig
