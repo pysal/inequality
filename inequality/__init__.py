4f33d37ee7bae214f88e3cf8cad5e4ffd531b004
Serge Rey <sjsrey@gmail.com>
2024-08-21 14:46:05 -0700

    Ruff

--- inequality/__init__.py
+++ inequality/__init__.py
@@ -8,14 +8,31 @@ import contextlib
 from importlib.metadata import PackageNotFoundError, version
 
 from . import atkinson, gini, schutz, theil
-from ._indices import (abundance, ellison_glaeser_egg, ellison_glaeser_egg_pop,
-                       fractionalization_gs, gini_gi, gini_gi_m, gini_gig,
-                       herfindahl_hd, hoover_hi, isolation_ii, isolation_isg,
-                       margalev_md, maurel_sedillot_msg,
-                       maurel_sedillot_msg_pop, menhinick_mi,
-                       modified_segregation_msg, polarization, segregation_gsg,
-                       shannon_se, similarity_w_wd, simpson_sd, simpson_so,
-                       theil_th)
+from ._indices import (
+    abundance,
+    ellison_glaeser_egg,
+    ellison_glaeser_egg_pop,
+    fractionalization_gs,
+    gini_gi,
+    gini_gi_m,
+    gini_gig,
+    herfindahl_hd,
+    hoover_hi,
+    isolation_ii,
+    isolation_isg,
+    margalev_md,
+    maurel_sedillot_msg,
+    maurel_sedillot_msg_pop,
+    menhinick_mi,
+    modified_segregation_msg,
+    polarization,
+    segregation_gsg,
+    shannon_se,
+    similarity_w_wd,
+    simpson_sd,
+    simpson_so,
+    theil_th,
+)
 
 with contextlib.suppress(PackageNotFoundError):
     __version__ = version("inequality")
