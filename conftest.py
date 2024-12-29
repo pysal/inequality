d1748438b24d7ba6c1c5176d05b6483743499a02
Serge Rey <sjsrey@gmail.com>
2024-12-29 11:13:52 -0800

    ruff

--- conftest.py
+++ conftest.py
@@ -7,21 +7,25 @@ import pytest
 
 def warning_depr(x):
     return pytest.warns(
-        FutureWarning, match=f"{x} is deprecated and will be removed on 2025-01-01."
+        FutureWarning,
+        match=f"{x} is deprecated and will be removed on 2025-01-01."
     )
 
+
 def warning_invalid(x):
-    return pytest.warns(RuntimeWarning, match=f"invalid value encountered in {x}")
+    return pytest.warns(RuntimeWarning,
+                        match=f"invalid value encountered in {x}")
 
-warning_div_zero = pytest.warns(RuntimeWarning, match="divide by zero encountered")
+
+warning_div_zero = pytest.warns(RuntimeWarning,
+                                match="divide by zero encountered")
 
 
-def pytest_configure(config):
+def pytest_configure():
     pytest.warning_depr = warning_depr
     pytest.warning_invalid = warning_invalid
     pytest.warning_div_zero = warning_div_zero
 
 
 def pytest_ignore_collect(collection_path):
-    return True if str(collection_path).endswith("_indices.py") else False
-
+    return bool(str(collection_path).endswith("_indices.py"))
